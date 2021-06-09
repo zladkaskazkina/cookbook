f16de0cb1347fabcc7e45561b51ae9d2ad0cc81a
Vladi Benesova <vladi.benesova1@gmail.com>
2021-06-09 19:00:38 +0200

    fix backend pathes

--- server/routes/GET/getIngredience.js
+++ server/routes/GET/getIngredience.js
@@ -1,6 +1,6 @@
 const getIngredients = require('express').Router();
 const ingredientModel = require('../../models/Ingredient');
-getIngredients.get('get-ingredients', (req,res) => {
+getIngredients.get('/get-ingredients', (req,res) => {
   ingredientModel.find({}, (err, data) => {
     if(err) {
       return res.json({
