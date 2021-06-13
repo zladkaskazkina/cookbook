16b78e17a035d106ceeca06a54eaad0fbce025ea
Vladi Benesova <vladi.benesova1@gmail.com>
2021-06-13 11:28:49 +0200

    improve backend saverecipe method

--- server/routes/POST/saveRecipe.js
+++ server/routes/POST/saveRecipe.js
@@ -1,22 +1,29 @@
 const saveRecipe = require('express').Router()
 const recipeModel = require('../../models/Recipe')
 
-saveRecipe.post('/save-recipe', (req, res) => {
-  const { author, category, name, summary, ingredients, preparationTime, method, photo, tags} = req.body;
+saveRecipe.post('/save-recipe', async (req, res) => {
+  const { category, name, ingredients, preparationTime, method, photo, tags} = req.body;
   const newRecipe = new recipeModel({
-    author, category, name, summary, ingredients, preparationTime, method, photo, tags
+    category, name, ingredients, preparationTime, method, photo, tags
   })
-  newRecipe.save((err, data) => {
-    if(err) {
-      return res.json({
-        msg: "Bohuzel nedoslo k ulozeni receptu"
-      })
-    } else {
-      return res.json({
+  try {
+    const data = await recipe.findOne({"name":name});
+
+    if(data) {
+      return res.status(409).json({
+        msg: "Recept s tímto jménem je už v naší databázi. Prosíme přejménovat recept"
+      });
+    }
+
+    const {_id} = await newRecipe.save();
+    if (_id) {
+      return res.status(201).json({
         msg: `Doslo k uspesnemu ulozeni receptu ${JSON.stringify(data)}`
       })
     }
-  })
+  } catch (error) {
+    res.status(500).json({msg: 'Bohužel došlo k chybě'});
+  }
 })
 
 module.exports = saveRecipe;
